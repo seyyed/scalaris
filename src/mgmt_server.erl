seyyed/scalaris

commit f201e30626ff189b0c71362b2470de60859dedfe

statistics: faster error handling if there is no mgmt_server
git-svn-id: https://scalaris.googlecode.com/svn/trunk@4559 71766da9-9752-0410-ab17-bfafa751c56a

src/mgmt_server.erl
@@ -25,7 +25,7 @@
 
 -export([start_link/2,
          number_of_nodes/0,
-         node_list/0,
+         node_list/0, node_list/1,
          connect/0]).
 
 -behaviour(gen_component).
@@ -66,11 +66,15 @@ connect() ->
 
 %% @doc trigger a message with all nodes known to the mgmt server
 -spec node_list() -> ok.
-node_list() ->
+node_list() -> node_list(false).
+
+-spec node_list(UseShepherd::boolean()) -> ok.
+node_list(UseShepherd) ->
     Pid = mgmtPid(),
+    SendOptions = ?IIF(UseShepherd, [{shepherd, self()}], []),
     case comm:is_valid(Pid) of
-        true -> comm:send(Pid, {get_list, comm:this()});
-        _    -> comm:send_local(self(), {get_list_response, []})
+        true -> comm:send(Pid, {get_list, comm:this()}, SendOptions);
+        _    -> comm:send_local(self(), {get_list_response, []}, SendOptions)
     end.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%

src/statistics.erl
@@ -81,9 +81,14 @@ get_memory({failed, _}) ->
 %%      within 2s.
 -spec get_ring_details() -> ring().
 get_ring_details() ->
-    mgmt_server:node_list(),
+    mgmt_server:node_list(true),
     Nodes = receive
-                ?SCALARIS_RECV({get_list_response, N}, N)
+                ?SCALARIS_RECV({get_list_response, N}, N);
+                ?SCALARIS_RECV({send_error, _, {get_list, _}, Reason},
+                               begin
+                                 log:log(error,"[ ST ] Mgmt server unavailable: ~p", [Reason]),
+                                 throw('mgmt_server_timeout')
+                               end)
             after 2000 ->
                 log:log(error,"[ ST ] Timeout getting node list from mgmt server"),
                 throw('mgmt_server_timeout')

commit 5ca0146a23a931de189bfc293f756e4ec6c1e102

mgmt_server: fix node_list/1 if no mgmt_server is configured
(regression in r4559)

src/mgmt_server.erl
@@ -71,10 +71,10 @@ node_list() -> node_list(false).
 -spec node_list(UseShepherd::boolean()) -> ok.
 node_list(UseShepherd) ->
     Pid = mgmtPid(),
-    SendOptions = ?IIF(UseShepherd, [{shepherd, self()}], []),
     case comm:is_valid(Pid) of
-        true -> comm:send(Pid, {get_list, comm:this()}, SendOptions);
-        _    -> comm:send_local(self(), {get_list_response, []}, SendOptions)
+        true -> comm:send(Pid, {get_list, comm:this()},
+                          ?IIF(UseShepherd, [{shepherd, self()}], []));
+        _    -> comm:send_local(self(), {get_list_response, []})
     end.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%